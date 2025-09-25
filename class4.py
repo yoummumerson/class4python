2fb8f4cfb74593c325e2906edf0a48be7b405727
yoummumerson <reyaansh.shoumik@gmail.com>
2025-09-25 20:16:52 +0900

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,16 @@
+"""for i in range(20,41,2):
+    print(i) Write a Python program for printing the table of 17 (1 to 10), using for loop.
+num = 17
+print("Table of 17")
+for i in range(1,11):
+    mul=num*i
+    print("17 x %d = %d" % (i, mul))
+    Write a Python program for printing the sum of the first ten natural numbers using the while loop.
+    num = 1
+sum = 0
+while(num<=100):
+    sum=sum+num
+    num=num+1
+print(sum)
+    Write a Python program to take a number as input from the user and check whether it is a prime number or not."""
+
